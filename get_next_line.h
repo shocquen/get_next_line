acff10a84f96bfd9e1935c7c5fbc929383540a6d
Souria-saky HOCQUENGHEM <shocquen@student.42.fr>
2021-12-02 16:43:00 +0100

    define BUFFER_SIZE

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: shocquen <shocquen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 12:23:30 by shocquen          #+#    #+#             */
-/*   Updated: 2021/12/02 14:53:57 by shocquen         ###   ########.fr       */
+/*   Updated: 2021/12/02 16:09:44 by shocquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 int		ft_strlen(const char *s);
 int		ft_strchr(const char *str, char c);
