6048042046075507f0460dd960c6862a83d91b7a
Souria-saky HOCQUENGHEM <shocquen@student.42.fr>
2021-12-02 14:55:43 +0100

    -D BUFFER_SIZE

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,12 @@
 /*   By: shocquen <shocquen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 12:23:30 by shocquen          #+#    #+#             */
-/*   Updated: 2021/12/02 14:34:14 by shocquen         ###   ########.fr       */
+/*   Updated: 2021/12/02 14:53:57 by shocquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# define BUFFER_SIZE 5
 
 # include <unistd.h>
 # include <stdlib.h>
